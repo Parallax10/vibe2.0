5e51251aee16031f4e94b95120a4f18368055fc9
Parallax10 <131243633+Parallax10@users.noreply.github.com>
2026-03-14 10:34:20 +0100

    pasar variables supabase a .env.local

--- src/app/utils/supabase.js
+++ src/app/utils/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://hulpxdzffljlomaulczi.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh1bHB4ZHpmZmxqbG9tYXVsY3ppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAzNzAzNzEsImV4cCI6MjA4NTk0NjM3MX0.mnhiRbNylVODrLPPdtA52A6jeL2gftTqcs_6R8IbVZE';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
