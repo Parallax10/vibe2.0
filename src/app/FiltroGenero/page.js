e67feefbb09d14473d5ed6ee382f18858237e05f
Parallax10 <131243633+Parallax10@users.noreply.github.com>
2026-03-14 11:12:23 +0100

    eliminar boton de prueba en paginas de filtros

--- src/app/FiltroGenero/page.js
+++ src/app/FiltroGenero/page.js
@@ -1,42 +1,17 @@
 "use client"
 import NavBar from "../navBar";
 import GridArtistas from "../GridArtistas";
-import { useState } from "react";
-export  default function Home() {
-    const [Admin,setAdmin]=useState(false)
-    const [texto,setTexto]=useState("Sugerir artista")
-    const [pagina,setPagina]=useState("")
-        function cambiarPerfil(){
-            setAdmin(!Admin)
-            if (Admin){
-                setTexto("Añadir artista")
-            }else{
-                setTexto("Sugerir artista")
-            }
-                
-        }
-        function accion(){
-            if (texto=="Añadir artista"){
-                setPagina("/NuevoArtista")
-            }else{
-                if (texto=="Sugerir artista"){
-                
-            }
-            }
-        }
+
+export default function Home() {
     return(
         <div>
-            <NavBar></NavBar>
-            <button onClick={()=>cambiarPerfil()}>Admin/User</button>
-            <a href={pagina} className="mx-5">
-                <button onClick={()=>accion()}>{texto}</button>
-            </a>
-        <div>
-            <h1 className="text-5xl">Filtro Genero</h1>
-            <hr></hr>
-            <br></br>
-            <GridArtistas modo="genero" size="large"></GridArtistas>
+            <NavBar pageType="filters" />
+            <div>
+                <h1 className="text-5xl">Filtro Genero</h1>
+                <hr></hr>
+                <br></br>
+                <GridArtistas modo="genero" size="large"></GridArtistas>
+            </div>
         </div>
-    </div>
-);
+    );
 }
--- src/app/FiltroPais/page.js
+++ src/app/FiltroPais/page.js
@@ -1,41 +1,16 @@
 "use client"
 import NavBar from "../navBar";
 import GridArtistas from "../GridArtistas";
-import { useState } from "react";
-export  default function Home() {
-    const [Admin,setAdmin]=useState(false)
-    const [texto,setTexto]=useState("Sugerir artista")
-    const [pagina,setPagina]=useState("")
-    function cambiarPerfil(){
-        setAdmin(!Admin)
-        if (Admin){
-            setTexto("Añadir artista")
-        }else{
-            setTexto("Sugerir artista")
-        }
-            
-    }
-    function accion(){
-        if (texto=="Añadir artista"){
-            setPagina("/NuevoArtista")
-        }else{
-            if (texto=="Sugerir artista"){
-            
-        }
-        }
-    }
-return(
-    <div>
-        <NavBar></NavBar>
-        <button onClick={()=>cambiarPerfil()}>Admin/User</button>
-        <a href={pagina} className="mx-5">
-            <button onClick={()=>accion()}>{texto}</button>
-        </a>
+
+export default function Home() {
+    return(
         <div>
-            <h1 className="text-5xl">Filtro por paises</h1>
-            <hr></hr>
-            <GridArtistas modo="pais" size="large"></GridArtistas>
+            <NavBar pageType="filters" />
+            <div>
+                <h1 className="text-5xl">Filtro por paises</h1>
+                <hr></hr>
+                <GridArtistas modo="pais" size="large"></GridArtistas>
+            </div>
         </div>
-    </div>
-);
+    );
 }
