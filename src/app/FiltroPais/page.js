00847ffb77801f91c0cff1dce44caa9248ac5f6b
Parallax10 <131243633+Parallax10@users.noreply.github.com>
2026-04-18 11:42:26 +0200

    fix suspense filtro pais

--- src/app/FiltroPais/page.js
+++ src/app/FiltroPais/page.js
@@ -1,16 +1,19 @@
 "use client"
+import { Suspense } from "react";
 import NavBar from "../navBar";
 import GridArtistas from "../GridArtistas";
 
 export default function Home() {
     return(
         <div>
-            <NavBar pageType="filters" />
-            <div>
-                <h1 className="text-5xl">Filtro por paises</h1>
-                <hr></hr>
-                <GridArtistas modo="pais" size="large"></GridArtistas>
-            </div>
+            <Suspense fallback={<div className="text-center mt-10">Cargando filtros...</div>}>
+                <NavBar pageType="filters" />
+                <div>
+                    <h1 className="text-5xl">Filtro por paises</h1>
+                    <hr></hr>
+                    <GridArtistas modo="pais" size="large"></GridArtistas>
+                </div>
+            </Suspense>
         </div>
     );
 }
