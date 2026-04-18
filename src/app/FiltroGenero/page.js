836b83696cfb62bb80e082a48aaeba5838442813
Parallax10 <131243633+Parallax10@users.noreply.github.com>
2026-04-18 11:40:20 +0200

    fix suspense filtro genero

--- src/app/FiltroGenero/page.js
+++ src/app/FiltroGenero/page.js
@@ -1,17 +1,20 @@
 "use client"
+import { Suspense } from "react";
 import NavBar from "../navBar";
 import GridArtistas from "../GridArtistas";
 
 export default function Home() {
     return(
         <div>
-            <NavBar pageType="filters" />
-            <div>
-                <h1 className="text-5xl">Filtro Genero</h1>
-                <hr></hr>
-                <br></br>
-                <GridArtistas modo="genero" size="large"></GridArtistas>
-            </div>
+            <Suspense fallback={<div className="text-center mt-10">Cargando filtros...</div>}>
+                <NavBar pageType="filters" />
+                <div>
+                    <h1 className="text-5xl">Filtro Genero</h1>
+                    <hr></hr>
+                    <br></br>
+                    <GridArtistas modo="genero" size="large"></GridArtistas>
+                </div>
+            </Suspense>
         </div>
     );
 }
